5a2fdc0c5f18704ccee2b393cc828451cd855436
JqkerN <ilian.corneliussen@gmail.com>
2020-02-10 23:07:43 +0100

    Progress bar

--- simpleProgressBar.py
+++ simpleProgressBar.py
@@ -1,13 +1,11 @@
-import time
 import numpy as np
 
 
 MIN = 0
-MAX = 99
-list = np.linspace(MIN,MAX,100)
+MAX = 99000
+list = np.linspace(MIN,MAX,100000)
 
 for elm in list:
-    progressBar = int(elm)*'#' + int(99-elm)*'-'
-    procents = " {0:.2f}%".format(100*elm/MAX)
+    progressBar = int(100*elm/MAX)*'#' + int(100-100*elm/MAX)*'-'
+    procents = "    {0:.2f}%".format(100*elm/MAX)
     print( progressBar + procents, end='\r', flush = True)
-    time.sleep(0.1)
